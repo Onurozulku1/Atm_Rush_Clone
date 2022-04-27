b0bac32c7b3eedb50c4368932e2cba5ac4ef0512
Onur <onurozulku1@gmail.com>
2022-04-28 01:23:05 +0300

    object pooler

--- Assets/Scripts/Game/ObjectPooler.cs
+++ Assets/Scripts/Game/ObjectPooler.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Pool
     {
-        
+        public string tag;
+        public GameObject prefab;
+        public int size;
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+
+    public static ObjectPooler instance;
+    private void Awake()
     {
-        
+        instance = this;
     }
+    
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag,Vector3 position,Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        return objectToSpawn;
+    }
+
 }

e53625ce3274957a4410d10d97183592f50e6060
Onur <onurozulku1@gmail.com>
2022-04-28 01:22:59 +0300

    clamp movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1;
     private Vector3 movementVector;
+    private Collider groundCollider;
+    private float groundBoundaries;
 
-    private void Update()
+    private void Awake()
     {
+        groundCollider = GameObject.FindGameObjectWithTag("Ground").GetComponent<Collider>();
+        groundBoundaries = groundCollider.bounds.extents.x * 0.8f;
     }
+
     private void FixedUpdate()
     {
-        movementVector = (Vector3.forward + Vector3.right * Input.GetAxis("Horizontal")) * speed * Time.deltaTime;
+        movementVector = (Vector3.forward + Vector3.right * Input.GetAxis("Horizontal")) * speed * 0.05f;
         transform.Translate(movementVector);
-        
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -groundBoundaries, groundBoundaries);
+        transform.position = clampedPosition;
     }
 }
